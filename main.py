e4e59d05d6e31eda9598f55874db7235f4c81584
fog <165981269+fog-e@users.noreply.github.com>
2024-05-29 17:49:09 +0000

    Add script to read and print contents of example_file.txt

---
+++ main.py
@@ -0,0 +1,11 @@
+# Open the file in read mode
+example_file = open("example_file.txt", "r")
+
+# Read the contents of the file
+file_contents = example_file.read()
+
+# Close the file
+example_file.close()
+
+# Print the contents of the file
+print(file_contents)
