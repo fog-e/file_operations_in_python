c85b50ec31155731bc4085948314430033a99e61
fog <165981269+fog-e@users.noreply.github.com>
2024-05-29 17:52:36 +0000

    Add append and read line by line operations

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-# Open the file in read mode
+# Read the contents of example_file.txt
 example_file = open("example_file.txt", "r")
-
-# Read the contents of the file
 file_contents = example_file.read()
-
-# Close the file
 example_file.close()
-
-# Print the contents of the file
 print(file_contents)
 
-# Open a new file in write mode
+# Write to new_file.txt
 new_file = open("new_file.txt", "w")
-
-# Write some content to the new file
 new_file.write("This is a new file with some content.\n")
-
-# Close the new file
 new_file.close()
 
-# Open the new file in read mode to verify its content
+# Read and print the contents of new_file.txt
 new_file = open("new_file.txt", "r")
 new_file_contents = new_file.read()
 new_file.close()
-
-# Print the contents of the new file
 print(new_file_contents)
+
+# Append to new_file.txt
+append_file = open("new_file.txt", "a")
+append_file.write("Appending new content.\n")
+append_file.close()
+
+# Read and print each line of new_file.txt
+line_file = open("new_file.txt", "r")
+for line in line_file:
+    print(line, end="")
+line_file.close()
