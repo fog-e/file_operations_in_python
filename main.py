1c07675d0142f1b1901d03f060e7f535db0f793f
fog <165981269+fog-e@users.noreply.github.com>
2024-05-29 17:51:40 +0000

    Add script to write to and read from new_file.txt

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ example_file.close()
 
 # Print the contents of the file
 print(file_contents)
+
+# Open a new file in write mode
+new_file = open("new_file.txt", "w")
+
+# Write some content to the new file
+new_file.write("This is a new file with some content.\n")
+
+# Close the new file
+new_file.close()
+
+# Open the new file in read mode to verify its content
+new_file = open("new_file.txt", "r")
+new_file_contents = new_file.read()
+new_file.close()
+
+# Print the contents of the new file
+print(new_file_contents)
